c8088d82fd6490ec950e148fa1f9bcc45817bff8
Taranveer Singh <taranveersingh93@gmail.com>
2023-02-07 04:00:33 -0500

    Display array elements

--- arrays.js
+++ arrays.js
@@ -4,3 +4,6 @@ booleanArray = [true, false, false, true]
 stringArray.pop() //removes last element
 numberArray.push(5) //adds 5 to the end of the array
 booleanArray.shift() //removes first element
+
+console.log(`The first element of the stringArray is ${stringArray[0]}`)
+console.log(`The last element of numberArray is ${numberArray[numberArray.length - 1]} `)
