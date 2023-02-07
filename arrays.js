85e73f672e1f5a75171c396fdd98aaa3ff88fd37
Taranveer Singh <taranveersingh93@gmail.com>
2023-02-07 03:52:50 -0500

    Include array functions

--- arrays.js
+++ arrays.js
@@ -1,3 +1,6 @@
 stringArray = ["alpha", "bravo", "charlie", "delta"]
 numberArray = [1, 2, 3, 4]
 booleanArray = [true, false, false, true]
+stringArray.pop() //removes last element
+numberArray.push(5) //adds 5 to the end of the array
+booleanArray.shift() //removes first element
